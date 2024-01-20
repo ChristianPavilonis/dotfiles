281407b81a23551ab0d62d09054716c55e2c1a5a
Christian <christianpavilonis@outlook.com>
2024-01-20 06:55:14 -0600

    Fixed harpoon keybind

--- nvim/lua/christian/plugins/harpoon.lua
+++ nvim/lua/christian/plugins/harpoon.lua
@@ -27,7 +27,7 @@ return {
     -- Keybinds
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 
-    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
     vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
     vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
