3174fe262b4cc257b76b6ab43defe09c8ff19228
Christian <christianpavilonis@outlook.com>
2025-03-31 19:29:44 -0500

    rust/js logging macros

--- nvim/lua/christian/plugins/macroni.lua
+++ nvim/lua/christian/plugins/macroni.lua
@@ -14,7 +14,19 @@ return {
         -- macro = 'vaco<Esc>f{byiw$%o<CR>impl<Space><Esc>pA<Space>{<CR><Esc>A<Tab>',
         keymap = '<leader>ri',
         desc = 'Rust: impl the current struct'
-      }
+      },
+
+      rust_visual_println = {
+        macro = 'yoprintln!(\"<Esc>pa:<Space>{:?}\",<Space><Esc>pa);<Esc>',
+        keymap = '<leader>rl',
+        desc = 'Rust: add a debug println! for the current visual selection'
+      },
+
+      javascript_visual_log = {
+        macro = 'yoconsole.log(\"<Esc>pa:\",<Space><Esc>pa);<Esc>',
+        keymap = '<leader>jl',
+        desc = 'JS: add a console.log for the current visual selection'
+      },
     }
   },
 }
